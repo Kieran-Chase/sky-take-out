ff5e689d15a0d67c07d20b7fcbaf1af3ce37235c
QiCheng Wang <2139317487@qq.com>
2026-02-10 23:16:37 +0800

    订单支付功能开发

--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -1,6 +1,8 @@
 package com.sky.service;
 
+import com.sky.dto.OrdersPaymentDTO;
 import com.sky.dto.OrdersSubmitDTO;
+import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderSubmitVO;
 
 /**
@@ -9,6 +11,23 @@ import com.sky.vo.OrderSubmitVO;
  * @date 2026/2/10
  */
 public interface OrderService {
-
+    /**
+     * 用户下单
+     * @param ordersSubmitDTO
+     * @return
+     */
     OrderSubmitVO submitOrder(OrdersSubmitDTO ordersSubmitDTO);
+
+    /**
+     * 订单支付
+     * @param ordersPaymentDTO
+     * @return
+     */
+    OrderPaymentVO payment(OrdersPaymentDTO ordersPaymentDTO) throws Exception;
+
+    /**
+     * 支付成功，修改订单状态
+     * @param outTradeNo
+     */
+    void paySuccess(String outTradeNo);
 }
