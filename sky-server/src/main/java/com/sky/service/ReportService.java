b39420b5cb6a1f8a91d038c77490a937b0e2667f
QiCheng Wang <2139317487@qq.com>
2026-02-12 16:35:43 +0800

    用户统计功能开发

--- sky-server/src/main/java/com/sky/service/ReportService.java
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -1,6 +1,7 @@
 package com.sky.service;
 
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -12,10 +13,20 @@ import java.time.LocalDateTime;
  */
 public interface ReportService {
     /**
-     * 营业额统计
+     * 统计指定时间区间内的营业额
      * @param begin
      * @param end
      * @return
      */
     TurnoverReportVO getTurnoverStatistics(LocalDate begin, LocalDate end);
+
+    /**
+     * 统计指定时间区间内的用户数据
+     * @param begin
+     * @param end
+     * @return
+     */
+    UserReportVO getUserStatistics(LocalDate begin, LocalDate end);
 }
+
+
